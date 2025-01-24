34a059b369419a62e500fd99e3bbb6da3e301cc8
eleddie <snchz26@gmail.com>
2025-01-24 21:29:03 +0100

    Improve team selector

--- cms/src/components/TeamSelector.tsx
+++ cms/src/components/TeamSelector.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { TeamDataType } from "../types";
 import { getTeams } from "../api";
-import { sortBy } from "lodash";
+import { kebabCase, sortBy } from "lodash";
 
 function TeamSelector() {
   const [teams, setTeams] = useState<TeamDataType[]>([]);
+  const [newTeam, setNewTeam] = useState<string>("");
 
   useEffect(() => {
     getTeams().then((data: TeamDataType[]) => {
@@ -14,21 +15,59 @@ function TeamSelector() {
     });
   }, []);
 
+  const onCreateTeam = () => {
+    if (!newTeam) return;
+    window.location.href = `/${kebabCase(newTeam)}`;
+  };
+
   return (
-    <div>
-      <h1 className="text-6xl font-semibold">Select your team</h1>
-      <br />
-      <select
-        className="text-xl p-2 rounded-md"
-        onChange={(e) => (window.location.href = `/${e.target.value}`)}
-      >
-        <option value="">Select a team</option>
-        {teams.map((team) => (
-          <option key={team.slug} value={team.slug}>
-            {team.name}
-          </option>
-        ))}
-      </select>
+    <div className="flex flex-row gap-16 justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-5xl font-semibold">Select your team</h2>
+
+        <br />
+
+        <select
+          className="text-xl p-2 rounded-md w-56"
+          onChange={(e) => (window.location.href = `/${e.target.value}`)}
+        >
+          <option value="">Select a team</option>
+          {teams.map((team) => (
+            <option key={team.slug} value={team.slug}>
+              {team.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div
+        style={{
+          width: "0.5px",
+          height: "200px",
+          background: "#777",
+        }}
+      ></div>
+
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-5xl font-semibold">Create a new team</h2>
+        <br />
+        <div className="flex flex-row gap-8">
+          <input
+            type="text"
+            placeholder="Team Name"
+            className="p-2 rounded-md"
+            onChange={(e) => setNewTeam(e.target.value)}
+            value={newTeam}
+          />
+
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={onCreateTeam}
+          >
+            Create
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
